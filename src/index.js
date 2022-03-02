adb7277ed5c733b813a27ff4cfd7365c556f1329
searchme86 <searchme86@gmail.com>
2022-03-02 20:23:06 +0900

    🏗️  redux structure added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './redux/reducers';
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider
+      store={createStoreWithMiddleware(
+        Reducer,
+        window.__REDUX_DEVTOOLS_EXPENSION &&
+          window.__REDUX_DEVTOOLS_EXPENSION__()
+      )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
