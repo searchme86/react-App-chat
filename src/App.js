a18309cdeac6653edad03937b8b20225fa201c5b
searchme86 <searchme86@gmail.com>
2022-03-02 20:07:21 +0900

    ⚡️ router pages created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ChatPage from './components/ChatPage/ChatPage';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ChatPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
