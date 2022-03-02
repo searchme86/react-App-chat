0bb49faedfc6fbb2c8084dc83bc88296389b668d
searchme86 <searchme86@gmail.com>
2022-03-02 21:13:35 +0900

    🔧  firebase config

---
+++ Firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
