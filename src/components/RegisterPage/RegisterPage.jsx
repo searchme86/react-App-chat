9b3e963f34832a17e8a91dc46e06e3301621eddb
searchme86 <searchme86@gmail.com>
2022-03-02 21:59:27 +0900

    💄 register page style

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function RegisterPage() {
   return (
     <div className="auth-wrapper">
+      <div style={{ textAlign: 'center' }}>
+        <h3>Register</h3>
+      </div>
       <form>
-        <input placeholder="First name" />
-        <select>
-          <option value="">Select...</option>
-          <option value="A">Option A</option>
-          <option value="B">Option B</option>
-        </select>
-        <textarea placeholder="About you" />
+        <label>Email</label>
+        <input type="email" name="email" placeholder="First name" />
+
+        <label>Name</label>
+        <input type="text" name="name" placeholder="First name" />
+
+        <label>Password</label>
+        <input type="password" name="password" placeholder="First name" />
+
+        <label>PasswordConfirm</label>
+        <input
+          type="passoword"
+          name="password_confirm"
+          placeholder="First name"
+        />
+
         <input type="submit" />
       </form>
+
+      <Link
+        style={{ color: 'gray', textDecoration: 'none', textAlign: 'left' }}
+        to="/login"
+      >
+        이미 아이디가 있다면...
+      </Link>
     </div>
   );
 }
