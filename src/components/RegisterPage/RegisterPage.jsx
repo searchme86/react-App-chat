189ecc1b4c51cfebc115614d59b2c9057b742133
searchme86 <searchme86@gmail.com>
2022-03-02 21:36:15 +0900

    💄 form style added

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 function RegisterPage() {
-  return <div>RegisterPage</div>;
+  return (
+    <div className="auth-wrapper">
+      <form>
+        <input placeholder="First name" />
+        <select>
+          <option value="">Select...</option>
+          <option value="A">Option A</option>
+          <option value="B">Option B</option>
+        </select>
+        <textarea placeholder="About you" />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPage;
