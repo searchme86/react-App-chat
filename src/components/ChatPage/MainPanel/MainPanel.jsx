adf67cb13d38aa71ee0bbeab28a53d2a98196b42
searchme86 <searchme86@gmail.com>
2022-03-05 20:11:38 +0900

    🐛  fixed find searched text

--- src/components/ChatPage/MainPanel/MainPanel.jsx
+++ src/components/ChatPage/MainPanel/MainPanel.jsx
@@ -11,6 +11,7 @@ import {
   off,
 } from 'firebase/database';
 import { connect } from 'react-redux';
+import MessageSearch from './MessageSearch';
 // import { setUserPosts } from '../../../redux/actions/chatRoom_action';
 // import Skeleton from '../../../commons/components/Skeleton';
 
@@ -58,18 +59,65 @@ class MainPanel extends Component {
     }
   }
 
+  //메세지를 입력해 검색하는 함수
+  //인풋에 입력한 텍스트와 비슷한 메세지를 찾는 로직
+  handleSearchMessages = () => {
+    const chatRoomMessages = [...this.state.messages];
+    //searchTerm : 인풋에 내가 입력한 텍스트들이 들어가는 변수, 찾고자 하는 것
+    //두번째, gi는 찾는 옵션임, g는 그것과 관련된 모든 찾는 글로벌적인 것을 match하고 i는 대문자 소문자 가리지 않고 모든 insensivive하게 모든 정보를 찾도록 돕는 옵션
+    const regex = new RegExp(this.state.searchTerm, 'gi');
+    const searchResults = chatRoomMessages.reduce((acc, message) => {
+      if (
+        (message.content && message.content.match(regex)) ||
+        message.user.name.match(regex)
+      ) {
+        acc.push(message);
+      }
+      return acc;
+    }, []);
+    this.setState({ searchResults });
+    //이후에 MessageBody로 로직이 이동한다.
+  };
+
+  //MessageHeader 컴포넌트에 함수를 props로 전달함
+  handleSearchChange = (event) => {
+    this.setState(
+      {
+        searchTerm: event.target.value,
+        searchLoading: true,
+      },
+      // 메세지를 입력하고 나서 바로 함수가 실행함
+      () => this.handleSearchMessages()
+    );
+  };
+
+  // renderMessages = (messages) => {
+  //   messages.length > 0 &&
+  //     messages.map((message) => {
+  //       return (
+  //         <ul style={{ paddingLeft: '-30px' }} key={this.props.user.id}>
+  //           <Message
+  //             key={this.props.user.id}
+  //             message={message}
+  //             user={this.props.user}
+  //           />
+  //         </ul>
+  //       );
+  //     });
+  // };
+
   render() {
     const {
       messages,
-      // searchTerm,
-      // searchResults,
+      searchTerm,
+      searchResults,
       // typingUsers,
       // messagesLoading,
     } = this.state;
 
     return (
       <div style={{ padding: '2rem 2rem 0 2rem' }}>
-        <MessageHeader />
+        <MessageHeader handleSearchChange={this.handleSearchChange} />
         <div
           style={{
             width: '100%',
@@ -80,18 +128,31 @@ class MainPanel extends Component {
             overflow: 'auto',
           }}
         >
-          {messages.length > 0 &&
-            messages.map((message) => {
-              return (
-                <ul style={{ paddingLeft: '-30px' }} key={this.props.user.id}>
-                  <Message
-                    key={this.props.user.id}
-                    message={message}
-                    user={this.props.user}
-                  />
-                </ul>
-              );
-            })}
+          {searchTerm
+            ? searchResults.length > 0 &&
+              searchResults.map((result) => {
+                return (
+                  <ul>
+                    <MessageSearch
+                      key={this.props.user.id}
+                      message={result}
+                      user={this.props.user}
+                    />
+                  </ul>
+                );
+              })
+            : messages.length > 0 &&
+              messages.map((message) => {
+                return (
+                  <ul style={{ paddingLeft: '-30px' }} key={this.props.user.id}>
+                    <Message
+                      key={this.props.user.id}
+                      message={message}
+                      user={this.props.user}
+                    />
+                  </ul>
+                );
+              })}
         </div>
         <MessageForm />
       </div>
